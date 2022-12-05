2e43255a3247b98a56904daeba492dd24cf7793b
mariusz-dabal <mariusz9dabal@gmail.com>
2022-12-05 18:01:58 +0100

    add Pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+import { createPinia } from "pinia"
 
 import "./assets/main.css"
 
@@ -19,5 +20,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(createPinia())
 
 app.mount("#app")
